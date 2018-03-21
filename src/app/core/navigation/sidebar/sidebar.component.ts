522c6b46a89ec5622e6efe3877c26da3ad0e59bf
Ishanz23 <sayantandey23@gmail.com>
2018-03-22 01:34:00 +0530

    sidebar edited for auth

--- src/app/core/navigation/sidebar/sidebar.component.ts
+++ src/app/core/navigation/sidebar/sidebar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../../../auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../../../app.reducer';
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,13 +12,21 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
   @Output() sidenavClose = new EventEmitter<void>();
-  constructor() { }
+  isAuth$: Observable<boolean>;
+  constructor(private router: Router, private authSvc: AuthService, private store: Store<fromRoot.State>) { }
 
   ngOnInit() {
+    this.isAuth$ = this.store.select(fromRoot.getIsAuthenticated);
   }
 
   onSidenavClose() {
     this.sidenavClose.emit();
+  }
+
+  onLogout() {
+    this.authSvc.logout();
+    this.router.navigate(['/']);
+    this.onSidenavClose();
   }
 
 }
