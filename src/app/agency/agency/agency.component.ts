7a7defbcc2969261de5cfab9af766c341e333b91
Ishanz23 <sayantandey23@gmail.com>
2018-03-26 10:37:27 +0530

    unsubscribe added to agencySub

--- src/app/agency/agency/agency.component.ts
+++ src/app/agency/agency/agency.component.ts
@@ -1,30 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestoreCollection, AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Agency } from '../../auth/agency.model';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
-import { User } from '@firebase/auth-types';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-agency',
   templateUrl: './agency.component.html',
   styleUrls: ['./agency.component.css']
 })
-export class AgencyComponent implements OnInit {
-  agencyCollection: AngularFirestoreCollection<Agency>;
+export class AgencyComponent implements OnInit, OnDestroy {
   agencyDoc: AngularFirestoreDocument<Agency>;
   agencyId: string;
-  agencies: Observable<Agency[]>;
-  agency: Observable<Agency>;
+  agency$: Observable<Agency>;
+  agencySub: Subscription;
   constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
-    this.afAuth.authState.subscribe(state => {
+    this.agencySub = this.afAuth.authState.subscribe(state => {
       this.agencyId = state.uid;
       this.agencyDoc = this.afs.doc(`testAgencies/${this.agencyId}`);
-      this.agency = this.agencyDoc.valueChanges();
+      this.agency$ = this.agencyDoc.valueChanges();
       // this.agencyDoc.valueChanges().subscribe(agent => console.log(agent));
     });
+  }
+
+  ngOnDestroy() {
+    this.agencySub.unsubscribe();
   }
 
 }
